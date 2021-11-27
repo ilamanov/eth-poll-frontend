bd93b689db45f4e64fc6df3600c169e638aa374f
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-11-27 10:08:53 +0200

    add 'Connect to MetaMask'

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -5,7 +6,62 @@ import styles from "../styles/Home.module.css";
 const TWITTER_HANDLE = "nazar_ilamanov";
 
 export default function Home() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const wave = () => {};
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +71,20 @@ export default function Home() {
       </Head>
 
       <header className={styles.header}>
+        <div className={styles.navBar}>
+          {currentAccount ? (
+            <div>
+              Connected with <Account address={currentAccount} />
+            </div>
+          ) : (
+            <button
+              className={styles.connectWalletButton}
+              onClick={connectWallet}
+            >
+              Connect to MetaMask
+            </button>
+          )}
+        </div>
         <div className={styles.avatarContainer}>
           <Image
             src="/avatar.jpeg"
@@ -56,5 +126,13 @@ export default function Home() {
         </a>
       </footer>
     </div>
+  );
+}
+
+function Account({ address }) {
+  return (
+    <span className={styles.address}>
+      {address.substring(0, 4)}…{address.substring(address.length - 4)}
+    </span>
   );
 }
