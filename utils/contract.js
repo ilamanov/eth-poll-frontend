8222db40eb307ee1fbd797bf7f28faf270bd5167
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-12-26 13:10:16 +0300

    update contract info

--- utils/contract.js
+++ utils/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import contractABI from "../utils/eth_poll_contract_abi.json";
 
-const CONTRACT_ADDRESS = "0x474212cb1D8E5973EDd82B2711aBCd9B886Da750";
+const CONTRACT_ADDRESS = "0x52c5724b2E3514cCc84cB7c977701A31e1d7C973";
 const CONTRACT_ABI = contractABI.abi;
 const NETWORK = "goerli";
 export const PROPOSE_COST = 0.0025;
