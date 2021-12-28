84dc1f878a70092455c71d747d8dccc1d77d85fe
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-12-28 22:44:36 +0300

    add account component

---
+++ components/ui/account.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import styles from "../../styles/components/ui/account.module.scss";
+
+function getAccountTextRepresentation(account, blockchain, isExpanded) {
+  if (isExpanded) {
+    return account;
+  }
+  if (blockchain === "ethereum") {
+    // take the first 5 and last 4 chars
+    return `${account.substring(0, 5)}…${account.substring(
+      account.length - 4
+    )}`;
+  } else if (blockchain === "solana") {
+    // take the first 4 and last 4 chars
+    return `${account.substring(0, 4)}…${account.substring(
+      account.length - 4
+    )}`;
+  }
+}
+
+export function Account({ account, isAccountMine, blockchain, wallet }) {
+  const [isExpanded, setIsExpanded] = useState("");
+
+  return (
+    <span
+      className={
+        styles.account +
+        " " +
+        (isAccountMine ? styles.isAccountMine : "") +
+        " " +
+        styles[wallet]
+      }
+      onClick={() => setIsExpanded(!isExpanded)}
+    >
+      {getAccountTextRepresentation(account, blockchain, isExpanded)}
+    </span>
+  );
+}
